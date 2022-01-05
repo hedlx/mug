18adff94b4c871a65d2973874047d363c44c7a62
Ilya Sumbatyants <ilya.sumb@gmail.com>
2022-01-05 20:00:23 +0700

    Try to close media session gracefully

--- src/mug/media_session.cljs
+++ src/mug/media_session.cljs
@@ -16,27 +16,42 @@
   (when-let [sess (.-mediaSession js/navigator)]
     (fun sess)))
 
-(defn sess-playing! []
-  (sess-apply! #(.then (fake-play) (fn [] (set! (.-playbackState %) "playing")))))
+(defonce callbacks (atom {}))
+
+(defn- set-session-meta! []
+  (sess-apply!
+   #(do (set! (.-metadata %)
+              (js/MediaMetadata.
+               (clj->js {:title "Symphony"
+                         :artist "Me"
+                         :album "Generative symphonies"
+                         :artwork [{:src (str (.-href js/location) "assets/cover.svg")
+                                    :type "image/svg+xml"}]}))))))
 
 (defn sess-paused! []
   (sess-apply! #(do (set! (.-playbackState %) "paused") (fake-pause))))
 
-(defn- set-session-meta! []
-  (sess-apply!
-   #(set! (.-metadata %)
-          (js/MediaMetadata.
-           (clj->js {:title "Symphony"
-                     :artist "Me"
-                     :album "Generative symphonies"
-                     :artwork [{:src (str (.-href js/location) "assets/cover.svg")
-                                :type "image/svg+xml"}]})))))
+(defn sess-playing! []
+  (sess-apply! #(.then (fake-play)
+                       (fn []
+                         (set-session-meta!)
+                         (.setActionHandler % "play" (fn [] ((:on-play @callbacks)) (sess-playing!)))
+                         (.setActionHandler % "pause" (fn [] ((:on-pause @callbacks)) (sess-paused!)))
+                         (set! (.-playbackState %) "playing")))))
+
+(defn- sess-stopped! []
+  (sess-apply! #(do (set! (.-playbackState %) "none") (fake-pause))))
 
 (defn set-callbacks! [on-play on-pause]
-  (sess-apply!
-   (fn [sess]
-     (.setActionHandler sess "play" #(do (on-play) (sess-playing!)))
-     (.setActionHandler sess "pause" #(do (on-pause) (sess-paused!))))))
+  (swap! callbacks assoc :on-play on-play)
+  (swap! callbacks assoc :on-pause on-pause))
 
 (defn setup-media-session! []
   (set-session-meta!))
+
+(.addEventListener js/window
+                   "beforeunload"
+                   (fn []
+                     (set! (.-src audio-el) "")
+                     (sess-apply! #(set! (.-metadata %) nil))
+                     (sess-stopped!)))
